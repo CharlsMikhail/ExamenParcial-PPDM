4bf085e4af2b3ee6dbd266cfb240425f9b26d69c
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-05-09 11:40:54 -0500

    Implmentamos un fragment más para la puntuacion:ScoreFragment. Diseñamos su interfaz.

---
+++ app/src/main/java/com/example/juegopreguntasrespuestaskotlin/ScoreFragment.kt
@@ -0,0 +1,15 @@
+package com.example.juegopreguntasrespuestaskotlin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class ScoreFragment : Fragment(R.layout.fragment_score) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
