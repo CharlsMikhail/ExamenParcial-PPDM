cc1dfd9ed96d8eca5b9863b42273d122c5227942
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-05-09 01:27:15 -0500

    Agregamos funcionalidad a el primer boton y corregimos el string.xml

--- app/src/main/java/com/example/juegopreguntasrespuestaskotlin/WelcomeFragment.kt
+++ app/src/main/java/com/example/juegopreguntasrespuestaskotlin/WelcomeFragment.kt
@@ -2,6 +2,8 @@ package com.example.juegopreguntasrespuestaskotlin
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,11 @@ class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val btnJugar = view.findViewById<Button>(R.id.btn_play)
+
+        btnJugar.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_questionFragment)
+        }
     }
 }
