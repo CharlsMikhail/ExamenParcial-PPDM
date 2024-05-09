f3acf616bf5aaf884af816e6fb48f45c4b7db974
Carlos Mijail Mamani Anccasi <89269010+CharlsMikhail@users.noreply.github.com>
2024-05-09 16:20:36 -0500

    proyecto completado!!, por el momento

--- app/src/main/java/com/example/juegopreguntasrespuestaskotlin/MainActivity.kt
+++ app/src/main/java/com/example/juegopreguntasrespuestaskotlin/MainActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 /**
- * @problemDescription Se solicita, realizar un juego de preguntas y respuestas usando fragments.
+ * @problemDescription Desarrollar  un juego de preguntas y respuestas en el que los usuarios puedan
+ * responder una serie de preguntas y obtener retroalimentación sobre sus respuestas. El juego
+ * consta de tres partes principales: la bienvenida al juego, la visualización de las preguntas y
+ * la visualización de las respuestas.
  * @author Carlos Mijail Mamani Anccasi
  * @creationDate 08/05/24
- * @lastModification 08/05/24
+ * @lastModification 09/05/24
  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
